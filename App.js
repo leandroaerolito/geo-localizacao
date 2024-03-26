0c9cc12c71327065bd3f7aa048f6ca17f398a9ee
Leandro Rocha <leandro.aerolito@gmail.com>
2024-03-26 09:12:04 -0300

    Abertura do mapa

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
 
 export default function App() {
+  const regiaoInicialMapa = {
+    latitude: -10,
+    longitude: -55,
+
+    /* Definição do zoom do mapa.
+    Quanto menor, mais próximo o mapa fica.
+    Quanto maior, mais longe o mapa fica */
+
+    latitudeDelta: 40,
+    longitudeDelta: 40,
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <View style={estilos.container}>
+        <MapView
+          mapType="standard"
+          style={estilos.mapa}
+          initialRegion={regiaoInicialMapa}
+          userInterfaceStyle="dark" //Somente iOS
+          maxZoomLevel={15}
+          minZoomLevel={5}
+        />
+      </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  mapa: { width: "100%", height: "100%" },
 });
